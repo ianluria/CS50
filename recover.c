65d7fe4122b25f5447516bbc5c25c64eb5ff57ce
Ian Luria <ian.luria@gmail.com>
2019-03-05 23:43:48 -0700

    Building out recover

--- recover.c
+++ recover.c
@@ -5,7 +5,11 @@
 #include <string.h>
 
 typedef uint8_t BYTE;
-typedef BYTE JPEGARRAY[512];
+typedef struct
+{
+    // Array of 512 bytes used to store a full jpeg
+    BYTE jpegArray[512];
+} JpegStorage;
 
 int main(int argc, char *argv[])
 {
@@ -26,17 +30,25 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-    bool jpegBlock = true;
+    bool nextJpegBlock = true;
 
-    while (jpegBlock)
+    while (nextJpegBlock)
     {
-        BYTE testByte1;
-        BYTE testByte2;
-        BYTE testByte3;
-        BYTE testByte4;
+        JpegStorage jpeg;
 
-        while (findJPEG)
+        FindJPEG(&jpeg);
+
+        if (jpeg.jpegArray[0] == 0){
+            nextJpegBlock = false;
+        }
+        else
+        {
+            //write jpegArray into new file
+        }
+        
     }
+
+    return 0;
 
     //while input has another 512 byte block
 
@@ -45,72 +57,94 @@ int main(int argc, char *argv[])
     // advance 512 bytes in input
 
     // if 512 block is less than 512 bytes, return false -- end of file
+}
 
-    //function findJPEG
+void function FindJPEG(JpegStorage jpegArrayPointer)
+{
+    BYTE testByte1;
+    BYTE testByte2;
+    BYTE testByte3;
+    BYTE testByte4;
+
+    size_t read1 = fread(&testByte1, sizeof(BYTE), 1, inptr);
+
+    if (read1 == 0)
     {
+        jpegArrayPointer[0] = 0;
+        return;
+    }
 
-        size_t read1 = fread(&testByte1, sizeof(BYTE), 1, inptr);
+    if (testByte1 == 255)
+    {
+        size_t read2 = fread(&testByte2, sizeof(BYTE), 1, inptr);
 
-        if (read1 == 0)
+        if (read2 == 0)
         {
-            //return error
+            jpegArrayPointer[0] = 0;
+            return;
         }
 
-        if (testByte1 == 255)
+        if (testByte2 == 216)
         {
-            size_t read2 = fread(&testByte2, sizeof(BYTE), 1, inptr);
+            size_t read3 = fread(&testByte3, sizeof(BYTE), 1, inptr);
 
-            if (read2 == 0)
+            if (read3 == 0)
             {
-                //return error
+                jpegArrayPointer[0] = 0;
+                return;
             }
 
-            if (testByte2 == 216)
+            if (testByte3 == 255)
             {
-                size_t read3 = fread(&testByte3, sizeof(BYTE), 1, inptr);
+                size_t read4 = fread(&testByte4, sizeof(BYTE), 1, inptr);
 
-                if (read3 == 0)
+                if (read4 == 0)
                 {
-                    //return error
+                    jpegArrayPointer[0] = 0;
+                    return;
                 }
 
-                if (testByte3 == 255)
+                if (testByte4 & 240 == 224)
                 {
-                    size_t read4 = fread(&testByte4, sizeof(BYTE), 1, inptr);
+                    //go back 4 spaces to the beginning of jpeg and add 512 bytes to array
+                    fseek(inptr, -4, SEEK_CUR);
 
-                    if (read4 == 0)
+                    size_t success = fread(jpegArrayPointer, sizeof(JPEGARRAY), 1, inptr);
+
+                    if (success < 512)
                     {
-                        //return error
+                        // The bytes read were less than 512, which means end of file was reached
+                        jpegArrayPointer[0] = 0;
                     }
 
-                    if (testByte4 & 240 == 224)
-                    {
-                        //go back 4 spaces and add 512 bytes to array
-                        //return array
-                    }
-                    else
-                    {
-                        fseek(inptr, 3, SEEK_CUR);
-                        //findJPG()
-                    }
+                    return;
                 }
                 else
                 {
-                    fseek(inptr, 2, SEEK_CUR);
+                    // Go back three pixels and rerun findJPEG
+                    fseek(inptr, -3, SEEK_CUR);
                     //findJPG()
                 }
             }
             else
             {
-                fseek(inptr, 1, SEEK_CUR);
+                // Go back two pixels and rerun findJPEG
+                fseek(inptr, -2, SEEK_CUR);
                 //findJPG()
             }
         }
         else
         {
-            // Run findJPG on the next pixel
+            // Go back one pixel and rerun findJPEG
+            fseek(inptr, -1, SEEK_CUR);
             //findJPG()
         }
-
-        return array[0] = 0;
     }
+    else
+    {
+        // Run findJPG on the next pixel
+        //findJPG()
+    }
+
+    return array[0] = 0;
+}
