ab8c673bc81dab7c0d11e9a5f088854d8f0a74d9
Ian Luria <ian.luria@gmail.com>
2019-03-07 18:11:03 -0700

    Checking how many blocks a jpeg will contain

--- recover.c
+++ recover.c
@@ -76,7 +76,9 @@ bool findJPEG(int *jpegBlockCounter, int counter, FILE *inptr)
 {
     BYTE testByte;
 
-    const int JPEGSIGNATUREBYTES[] = {255, 216, 255};
+    //bool foundJPEG = true;
+
+    
 
     while (testByte == 0)
     {
@@ -89,63 +91,149 @@ bool findJPEG(int *jpegBlockCounter, int counter, FILE *inptr)
         }
     }
 
-    // End of JPEGSIGNATUREBYTES array reached
-    if (counter == 3)
-    {
-        if ((testByte & 240) == 224)
+        //advanced to a non-zero testByte
+
+        bool signatureFound = testForJPEGSignature(0, *inptr);
+
+       
+
+        if (signatureFound)
         {
-            //go back 4 spaces to the beginning of jpeg and add 512 bytes to array
-            fseek(inptr, -4, SEEK_CUR);
 
-            *jpegBlockCounter = 1;
+            
+             fseek(inptr, -4, SEEK_CUR);
 
-            bool anotherBlock = true;
-
-            while (anotherBlock)
-            {
-                fseek(inptr, 512, SEEK_CUR);
-
-                //seek to end of block
-                //check if block signautre is present
-                //if so, increment blockcounter
-                //else end function and return to main
-            }
-
-            return true;
         }
+
+        bool nextBlock = true;
+
+
+        while (nextBlock)
+        {
+            if (testForJPEGSignature(0, *inptr))
+            {   
+                //fseek back to beginning of block
+                nextBlock = false;
+            }
+            else
+            {
+                //fseek back to beginning of block
+                //read 512
+            }
+            
+
+
+
+
+        }
+
+        //check to see if the next block begins with a signature
+        //if it does, stop
+        //else, advance block count
+
+
+ //fread
+            fseek(inptr, 512, SEEK_CUR);
+*jpegBlockCounter += 1;
+
+        //test if next block begins with signature
+
+        bool anotherBlock = true;
+
+        while (anotherBlock)
+        {
+           if (testForJPEGSignature(0, *inptr))
+           {
+            
+           }
+
+            //need to change these to fread and not fseek
+
+           else
+           {
+            *jpegBlockCounter += 1;
+
+            fseek(inptr, -4, SEEK_CUR);
+           
+           //fread
+            fseek(inptr, 512, SEEK_CUR);
+           }
+           
+
+            //seek to end of block
+            //check if block signautre is present
+            //if so, increment blockcounter
+            //else end function and return to main
+        }
+
+        return true;
     }
-    else if (testByte == JPEGSIGNATUREBYTES[counter])
-    {
-        counter += 1;
+}
+else if (testByte == JPEGSIGNATUREBYTES[counter])
+{
+    counter += 1;
 
-        findJPEG(jpegStoragePointer, counter, inptr);
-    }
+    findJPEG(jpegStoragePointer, counter, inptr);
+}
 
-    if (counter > 0)
-    {
-        int negativeCounter = counter * -1;
+if (counter > 0)
+{
+    int negativeCounter = counter * -1;
 
-        // Go back in file negativeCounter spaces
-        fseek(inptr, negativeCounter, SEEK_CUR);
-    }
+    // Go back in file negativeCounter spaces
+    fseek(inptr, negativeCounter, SEEK_CUR);
+}
 
-    findJPEG(jpegStoragePointer, 0, inptr);
+findJPEG(jpegStoragePointer, 0, inptr);
 
-    return false;
+return false;
 }
 
 //Given the current location in file, check if the next four bytes are a jpeg signature
 
+//NEED TO ADD ERROR CHECKING FOR FREAD!!!!!!!!!!
 
+bool testForJPEGSignature(int index, FILE *inptr)
+{
+    const int JPEGSIGNATUREBYTES[] = {255, 216, 255};
 
+    BYTE testByte;
 
+    size_t read = fread(&testByte, sizeof(BYTE), 1, inptr);
 
+    if (index == 3)
+    {
+        if ((testByte & 240) == 224)
+        {
 
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
+    if (testByte == JPEGSIGNATUREBYTES[index])
+    {
+        index += 1;
+        bool foundNextSignatureByte = testForJPEGSignature(index, inptr);
 
+        if (foundNextSignatureByte)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
-
-
+    // Go back in file and try again
+    fseek(inptr, (index * -1), SEEK_CUR);
+    
+    return false;
+}
 
 // size_t success = fread(jpegStoragePointer->jpegArray, sizeof(jpegStoragePointer->jpegArray), 1, inptr);
 
