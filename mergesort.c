67e40c54f99aeab1e377d33461eefa5ef44ebda8
Ian Luria <ian.luria@gmail.com>
2019-03-30 14:31:05 -0600

    Added comments

--- mergesort.c
+++ mergesort.c
@@ -5,15 +5,18 @@ void merge(int low, int high, int *array, int *aux);
 
 int main(void)
 {
-
+    // Array that will be sorted
     int array[] = {25, 10, 8, 6};
 
     int length = (sizeof(array) / sizeof(array[0]));
 
+    // Temporary array used to store values during sorting
     int aux[length];
 
+    // Copy contents of array
     memcpy(aux, array, sizeof(array));
 
+    // Begin recursive process
     merge(0, length - 1, array, aux);
 
     for (int i = 0; i < length; i++)
@@ -24,9 +27,10 @@ int main(void)
     return 0;
 }
 
+// Takes min and max indexes and writes ascendingly the corresponding array values.
 void merge(int low, int high, int *array, int *aux)
 {
-
+    // Base case is reached when there is only one value
     if (low == high)
     {
         return;
@@ -34,21 +38,26 @@ void merge(int low, int high, int *array, int *aux)
 
     int midpoint = ((high - low) / 2) + low;
 
+    // Break down range by midpoint until base case is reached
     merge(low, midpoint, array, aux);
     merge(midpoint + 1, high, array, aux);
 
-    //combine the two arrays
+    // Combine the two ranges in ascending order
 
+    //Index at which range begins
     int leftPosition = low;
     int rightPosition = midpoint + 1;
 
+    //For each index in range, insert the value by order from array into aux
     for (int i = low; i <= high; i++)
     {
+        // leftPosition has passed its limit
         if (leftPosition > midpoint)
         {
             aux[i] = array[rightPosition];
             rightPosition += 1;
         }
+        // rightPosition has passed its limit
         else if (rightPosition > high)
         {
             aux[i] = array[leftPosition];
@@ -66,6 +75,7 @@ void merge(int low, int high, int *array, int *aux)
         }
     }
 
+    // Adjust the values in array once new positions in aux are settled
     for (int i = low; i <= high; i++)
     {
         array[i] = aux[i];
